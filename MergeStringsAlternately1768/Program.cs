5ddd9197691a95ec0e551546fda65d7c93e9fca4
Mohammadabuhammad <abuhammad2001@gmail.com>
2024-11-05 17:03:19 +0300

    1768. Merge Strings Alternately

---
+++ MergeStringsAlternately1768/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MergeStringsAlternately1768
+{
+    internal class Program
+    {
+        public string MergeAlternately(string word1, string word2)
+        {
+            string MergedWord = "";
+            int pointer = 0 , x = word1.Length, y = word2.Length;
+
+            while(pointer < x || pointer < y)
+            {
+                if (pointer < x)
+                    MergedWord += word1[pointer];
+                if (pointer < y)
+                    MergedWord += word2[pointer];
+                
+                pointer++;
+            }
+            return MergedWord;
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            // input string
+
+            string word1 = "ace";
+            string word2 = "bdf";
+
+            string result = program.MergeAlternately(word1, word2);
+
+            Console.WriteLine(result);
+        }
+    }
+}
